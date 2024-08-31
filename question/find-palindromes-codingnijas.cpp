f7be5390ee07a9d2b7b45c4b5fe95029368f5420
kinish123 <kinishneema7723@gmail.com>
2024-08-31 18:19:31 +0530

    Find Palindromes coding ninjas

---
+++ question/find-palindromes-codingnijas.cpp
@@ -0,0 +1,27 @@
+// #include <bits/stdc++.h> 
+// vector<int> getPalindromes(int n)
+// {
+// 	// Write your code here
+// 	vector<int> temp;
+// 	for(int i=1;i<=n;i++){
+// 		if(i<10 ){
+// 			temp.push_back(i);
+// 		}
+// 		else{
+// 			int newi=i;
+// 			int sum=0;
+// 			while(newi>0){
+// 			int mod=newi%10;
+// 			sum=sum*10+mod;
+// 			newi=newi/10;
+// 		}
+// 			if(sum==i) {
+// 				temp.push_back(i);
+// 			}
+			
+			
+// 		}
+// 	}
+// 	return temp;
+// }
+
